0b060f36239f6a5553976106a3679941c2b29bae
Maxence <max@23.tf>
2022-06-29 00:54:29 +0000

    Fix wordlist fetch

--- wordlepy/__init__.py
+++ wordlepy/__init__.py
@@ -28,19 +28,19 @@ class Wordle:
             if len(self.words) <= 1:
                 print("Congrats, you found the solution '{}' in {} guesses!".format(word.upper(), self.guesses))
                 break
-        
+
     def getLetterFreqs(self):
         return [
             dict(Counter([word[i] for word in self.words]).most_common())
         for i in range(5)]
-    
+
     def getWordsList(self):
         mainpage = requests.get("https://www.nytimes.com/games/wordle/index.html")
         m = re.search(re.compile('src="https://www\.nytimes\.com/games-assets/v2/wordle\.([^"]+).js"'), mainpage.content.decode())
         data = requests.get("https://www.nytimes.com/games-assets/v2/wordle.{}.js".format(m.group(1)))
-        m = re.search(re.compile("var z=(\\[[^\\]]+\\])"), data.content.decode())
+        m = re.search(re.compile("var [A-Za-z]=(\\[[^\\]]+\\])"), data.content.decode())
         self.words = json.loads(m.group(1))
-        
+
     # We clean up the list of words we currently have, to remove the latest try and clean up based on feedback
     # eg filterOut(words, "slate", "*latE") means we got the E right, lat wonr location and s wrong
     def updateLetterSets(self, tried, response):
